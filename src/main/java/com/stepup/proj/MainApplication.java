b70fbad5f2e6e9b52260bbced5bfa9aa38ec1dbc
Andrey Sender <ansen80@inbox.ru>
2024-08-13 18:47:14 +0300

    Доп исправление 08

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -3,14 +3,14 @@ package com.stepup.proj;
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("1---");
+        System.out.println("1-");
     }
 
     public static void doSomething() {
         System.out.println(1);
         System.out.println(2);
         System.out.println(9);
-        System.out.println(9);
+        System.out.println(10);
     }
 }
 
